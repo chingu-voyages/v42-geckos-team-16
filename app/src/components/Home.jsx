85dfce58de1d6ed10b474a2b8198f8882a5d0b73
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2023-01-18 21:21:58 +0300

    Navbar
    I create Navbar by using bootstarp I create Navbar on small screen and hide it on big and medium screen and did the opposite on big and medium screen

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -2,47 +2,49 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { firstImage, secondImage, thirdImage } from "../images/images";
 import CarouselItem from "./Carousel";
+import Header from "./navbarComponent/Header";
 
 const images = [
-    {
-        src: firstImage,
-        heading: "Shop with us",
-        name: "Watch",
-        description: "A wide variety of products at your disposal",
-    },
-    {
-        src: secondImage,
-        heading: "Find your joy",
-        name: "Speaker",
-        description: "We sell everything you need",
-    },
-    {
-        src: thirdImage,
-        heading: "New Year's Promotion",
-        name: "Earphone",
-        description: "Browse through our rich catalogues",
-    },
+  {
+    src: firstImage,
+    heading: "Shop with us",
+    name: "Watch",
+    description: "A wide variety of products at your disposal",
+  },
+  {
+    src: secondImage,
+    heading: "Find your joy",
+    name: "Speaker",
+    description: "We sell everything you need",
+  },
+  {
+    src: thirdImage,
+    heading: "New Year's Promotion",
+    name: "Earphone",
+    description: "Browse through our rich catalogues",
+  },
 ];
 
 export const Home = () => {
-    return (
-        <Carousel slide={false}>
-            {images.map((image, _index) => {
-                return (
-                    <Carousel.Item key={new Date() * _index} className="m-0">
-                        <CarouselItem
-                            imgsrc={image.src}
-                            description={image.description}
-                            alt={image.name}
-                            heading={image.heading}
-                        >
-                            <button className="btn btn-primary">
-                                Get Started
-                            </button>
-                        </CarouselItem>
-                    </Carousel.Item>
-                );
-            })}
-        </Carousel>
-    );
+  return (
+    <div>
+      <Header />
+      <Carousel slide={false}>
+        {images.map((image, _index) => {
+          return (
+            <Carousel.Item key={new Date() * _index} className="m-0">
+              <CarouselItem
+                imgsrc={image.src}
+                description={image.description}
+                alt={image.name}
+                heading={image.heading}
+              >
+                <button className="btn btn-primary">Get Started</button>
+              </CarouselItem>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
 };
---
+++ app/src/components/navbarComponent/navbarElement/NavbarB.js
@@ -0,0 +1,58 @@
+import "./navbar.css";
+// Navbar on Big Screen
+
+import React from "react";
+
+function NavbarOnBigScreen() {
+  const navItems = [
+    {
+      name: "Home",
+      pageLink: "#",
+    },
+    {
+      name: "Product",
+      pageLink: "#",
+    },
+    {
+      name: "About Us",
+      pageLink: "#",
+    },
+    {
+      name: "Contact Us",
+      pageLink: "#",
+    },
+  ];
+
+  const displayNavItems = navItems.map((el) => {
+    return (
+      <li className="nav-item ">
+        <a
+          className="text-dark nav-link active link-color "
+          aria-current="page"
+          href={el.pageLink}
+        >
+          {el.name}
+        </a>
+      </li>
+    );
+  });
+
+  return (
+    <div className="displayNavbarOnBigScreen">
+      <div className="d-flex align-items-center justify-content-between navbar-container">
+        <div>
+          <h1>Premium Team</h1>
+        </div>
+        <div className="d-flex justify-content-between align-items-center iconsBTN">
+          <i class="ri-book-3-line fs-5 iconBTN"></i>
+          <i className="ri-user-line fs-5 iconBTN"></i>
+          <i className="ri-shopping-bag-line fs-5 iconBTN"></i>
+        </div>
+      </div>
+      <br />
+      <ul class="nav md justify-content-center">{displayNavItems}</ul>
+    </div>
+  );
+}
+
+export default NavbarOnBigScreen;

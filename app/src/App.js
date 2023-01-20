0ec04dd5c83001bd55a06255e15b70db64248bc0
Ianel Tombozafy <ianell.s118@gmail.com>
2023-01-20 09:53:35 +0300

    add product details route

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import Header from "./components/navbarComponent/Header";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/products/:id" element={<ProductDetails />} />
                 </Routes>
             </Router>
         </>
---
+++ app/src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductDetails = () => {
+    return (
+        <div className="w-75 container-fluid">
+            <div className="w-50"></div>
+        </div>
+    );
+};
+
+export default ProductDetails;

c0bdfbf252e74dfe82c49503b501a74adc7c179e
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2023-01-21 19:21:51 +0300

    cart page design + small adjustment in header
    I create cart page design and adjustment the header component to use less code for same result

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Cart } from "./components/Cart/Cart";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import Header from "./components/navbarComponent/Header";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 
 function App() {
-    return (
-        <>
-            <Header />
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/products/:id" element={<ProductDetails />} />
-                </Routes>
-            </Router>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/order" element={<Cart />} />
+
+          <Route path="/products/:id" element={<ProductDetails />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- app/src/components/navbarComponent/navbarElement/NavbarB.js
+++ app/src/components/navbarComponent/navbarElement/NavbarB.js
@@ -3,56 +3,23 @@ import "./navbar.css";
 
 import React from "react";
 
-function NavbarOnBigScreen() {
-    const navItems = [
-        {
-            name: "Home",
-            pageLink: "/",
-        },
-        {
-            name: "Product",
-            pageLink: "#",
-        },
-        {
-            name: "About Us",
-            pageLink: "#",
-        },
-        {
-            name: "Contact Us",
-            pageLink: "#",
-        },
-    ];
+function NavbarOnBigScreen(props) {
+  const { navItemsDesplay, displayBTN } = props;
 
-    const displayNavItems = navItems.map((el, index) => {
-        return (
-            <li className="nav-item" key={new Date() * index}>
-                <a
-                    className="text-dark nav-link active link-color "
-                    aria-current="page"
-                    href={el.pageLink}
-                >
-                    {el.name}
-                </a>
-            </li>
-        );
-    });
-
-    return (
-        <div className="displayNavbarOnBigScreen">
-            <div className="d-flex align-items-center justify-content-between navbar-container">
-                <div>
-                    <h1>Premium Team</h1>
-                </div>
-                <div className="d-flex justify-content-between align-items-center iconsBTN">
-                    <i className="ri-book-3-line fs-5 iconBTN"></i>
-                    <i className="ri-user-line fs-5 iconBTN"></i>
-                    <i className="ri-shopping-bag-line fs-5 iconBTN"></i>
-                </div>
-            </div>
-            <br />
-            <ul className="nav md justify-content-center">{displayNavItems}</ul>
+  return (
+    <div className="displayNavbarOnBigScreen">
+      <div className="d-flex align-items-center justify-content-between navbar-container">
+        <div>
+          <h1>Premium Team</h1>
         </div>
-    );
+        <div className="d-flex justify-content-between align-items-center iconsBTN">
+          {displayBTN}
+        </div>
+      </div>
+      <br />
+      <ul className="nav md justify-content-center">{navItemsDesplay}</ul>
+    </div>
+  );
 }
 
 export default NavbarOnBigScreen;

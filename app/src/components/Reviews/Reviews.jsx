1ed6f6a8987175c0a08cd0a7e6dfe637bab57e07
Ianel Tombozafy <ianell.s118@gmail.com>
2023-01-21 04:27:28 +0300

    add link to register page from login

--- app/src/components/Reviews/Reviews.jsx
+++ app/src/components/Reviews/Reviews.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Reviews = () => {
-    return <div>Reviews</div>;
+    return (
+        <div className="border border-2">
+            <h2 className="text-uppercase">Customer reviews</h2>
+        </div>
+    );
 };
 
 export default Reviews;

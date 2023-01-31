2f5aada50329d0dde6b4a959a9f549789ace9be8
Ianel Tombozafy <ianell.s118@gmail.com>
2023-01-31 13:57:58 +0300

    add star icons rate

--- app/src/components/ProductDetails/ProductDetails.jsx
+++ app/src/components/ProductDetails/ProductDetails.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { formatString } from "../../helpers/string.helper";
 import { firstImage, secondImage } from "../../images/images";
 import { StarFillIcon } from "../Icons/Icon";
 import StarIcons from "../Icons/StarIcons.jsx/StarIcons";
@@ -58,13 +59,16 @@ const ProductDetails = () => {
     const [image, setImage] = useState(0);
     const [quantity, setQuantity] = useState(1);
     const [imgIndex, setImgIndex] = useState(0);
+    const [product, setProduct] = useState({});
 
     const { id } = useParams();
 
     const fetchProduct = async () => {
         const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-        const product = await response.json();
-        console.log(product);
+        const data = await response.json();
+        setProduct(data);
+
+        console.log(data);
     };
 
     useEffect(() => {
@@ -76,7 +80,7 @@ const ProductDetails = () => {
             <div className="row mx-1">
                 <div className="col-md">
                     <img
-                        src={images[image]["img"]}
+                        src={product["image"]}
                         alt="First Image"
                         className="img-fluid"
                         style={{
@@ -84,7 +88,7 @@ const ProductDetails = () => {
                             objectFit: "cover",
                         }}
                     />
-                    <div className="my-4 d-flex d-md-block justify-content-center">
+                    {/* <div className="my-4 d-flex d-md-block justify-content-center">
                         {images.map((img, index) => {
                             return (
                                 <img
@@ -107,16 +111,18 @@ const ProductDetails = () => {
                                 />
                             );
                         })}
-                    </div>
+                    </div> */}
                 </div>
                 <div className="col-md">
                     <div className="mx-lg-5">
-                        <h2 className="fw-light">
-                            Harney & Sons HRP Valentine's Day Tea (30 Sachets)
-                        </h2>
-                        <h3 className="fs-5 fw-bold">$24.95</h3>
-                        <StarIcons />
-                        <p>Shipping calculated at checkout</p>
+                        <h2 className="fw-light">{product["title"]}</h2>
+                        <h3 className="fs-5 fw-bold">${product["price"]}</h3>
+                        <StarIcons rate={product && product.rating?.rate} />
+                        {/* <p>Shipping calculated at checkout</p> */}
+                        <h3 className="fs-5 text-primary">
+                            {product["category"] &&
+                                formatString(product["category"])}
+                        </h3>
                         <p className="mt-5">Quantity</p>
                         <input
                             type="number"
@@ -137,18 +143,11 @@ const ProductDetails = () => {
                     </div>
                     <div className="mx-2">
                         <p>
-                            Surprise your Valentine with the exclusive Harney &
-                            Sons Valentine's Day tea. It is a romantic brew of
-                            chocolate flavoured Chinese black tea blended with
-                            vanilla and fragrant rosebuds. A perfect way to show
-                            your love and share it with your special someone in
-                            the cozy winter season... or why not throughout the
-                            year. This may be your next favourite tea. This tin
-                            contains 30 silken sachets and makes the perfect
-                            gift
+                            {product["description"] &&
+                                formatString(product["description"])}
                         </p>
                     </div>
-                    <div className="col hidden d-lg-block">
+                    {/* <div className="col hidden d-lg-block">
                         <p>
                             <span className="fw-bold">Ingredients:</span> Black
                             tea, rosebuds, chocolate flavour, vanilla flavour.
@@ -169,10 +168,10 @@ const ProductDetails = () => {
                                 })}
                             </tbody>
                         </table>
-                    </div>
+                    </div> */}
                 </div>
             </div>
-            <div className="row mx-3 d-none d-md-block d-lg-none">
+            {/* <div className="row mx-3 d-none d-md-block d-lg-none">
                 <p>
                     <span className="fw-bold">Ingredients:</span> Black tea,
                     rosebuds, chocolate flavour, vanilla flavour. Contains
@@ -190,8 +189,8 @@ const ProductDetails = () => {
                         })}
                     </tbody>
                 </table>
-            </div>
-            <Reviews />
+            </div> */}
+            {/* <Reviews /> */}
         </div>
     );
 };

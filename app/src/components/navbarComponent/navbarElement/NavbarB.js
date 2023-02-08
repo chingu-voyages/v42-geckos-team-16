e708b3ac4a286883cb87498b94a423b344066da4
ordegani <oradeganii@gmail.com>
2023-02-08 23:31:13 +0200

    add popup specific content

--- app/src/components/navbarComponent/navbarElement/NavbarB.js
+++ app/src/components/navbarComponent/navbarElement/NavbarB.js
@@ -1,12 +1,10 @@
 import "./navbar.css";
 import { BASE_URL } from "../../../constants/urls";
-// import { BASE_URL } from "../../constants/urls";
 import axios from "axios";
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-// Navbar on Big Screen
 import { useEffect, useState } from "react";
-
+// Navbar on Big Screen
 import React from "react";
 
 function NavbarOnBigScreen() {
@@ -48,17 +46,15 @@ function NavbarOnBigScreen() {
     );
   });
   const [data, setData] = useState([""])
-
   useEffect(() => {
-  const fetchUserInfo = async () => {
-    const data = await axios.get("https://store-api-81pb.onrender.com/api/v1/users");
-    setData(data.data);
-    console.log(data.data);
-  };
+    const fetchUserInfo = async () => {
+      const data = await axios.get("https://store-api-81pb.onrender.com/api/v1/users");
+      setData(data.data);
+      console.log(data.data);
+    };
     fetchUserInfo();
   }, [])
 
-  // const userData = (data);
   return (
     <div className="displayNavbarOnBigScreen">
       <div className="d-flex align-items-center justify-content-between navbar-container">
@@ -67,26 +63,17 @@ function NavbarOnBigScreen() {
         </div>
         <div className="d-flex justify-content-between align-items-center iconsBTN">
           <i className="ri-book-3-line fs-5 iconBTN"></i>
-          {/* TODO add likn to user information */}
           <Popup
             trigger={open => (
               <i className="ri-user-line fs-5 iconBTN">
               </i>
             )}
-            //     position="bottom center"
             closeOnDocumentClick
           >
             <span>
-   <p>Status: {data.status}</p>
-            {/* {data.map((dataItem) => {
-        return (
-          <div className="Popup">
-            <h2>{dataItem.email}</h2>
-          </div>
-        )})} */}
+              <p>Status: {data.status}</p>
             </span>
           </Popup>
-
           <i className="ri-shopping-bag-line fs-5 iconBTN"></i>
         </div>
       </div>

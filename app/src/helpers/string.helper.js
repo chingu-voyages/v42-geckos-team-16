95214dcac553d027aad9fb6aed56464e74a75464
Egnoel <egnoel@hotmail.com>
2023-02-17 18:37:55 +0100

    removing the second app folder

--- app/src/helpers/string.helper.js
+++
@@ -1,2 +0,0 @@
-export const formatString = (str) =>
-    str.charAt(0).toUpperCase() + str.slice(1, str.length);
--- app/src/images/images.js
+++
@@ -1,5 +0,0 @@
-import firstImage from "./watch.jpg";
-import secondImage from "./speaker.jpg";
-import thirdImage from "./earphone.jpg";
-
-export { firstImage, secondImage, thirdImage };
